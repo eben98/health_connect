e41fcb771d83ce068a4acae2e30c655986bb05ba
Ebenezer Daniel <ebenezerdaniel98@gmai.com>
2025-07-17 21:30:32 -0500

    Launcher commit

--- lib/health_page.dart
+++ lib/health_page.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:health/health.dart';
 
-class HealthPage extends StatefulWidget {
-  const HealthPage({super.key});
+class StepCounterScreen extends StatefulWidget {
+  const StepCounterScreen({super.key});
 
   @override
-  State<HealthPage> createState() => _HealthPageState();
+  _StepCounterScreenState createState() => _StepCounterScreenState();
 }
 
-class _HealthPageState extends State<HealthPage> {
-  // final HealthFactory health = HealthFactory();
+class _StepCounterScreenState extends State<StepCounterScreen> {
+  List<HealthDataPoint> _stepData = [];
+  final Health _health = Health();
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchStepData();
+  }
+
+  Future<void> _fetchStepData() async {
+    try {
+      List<HealthDataType> types = [HealthDataType.STEPS];
+      DateTime now = DateTime.now();
+      DateTime start = DateTime(now.year, now.month, now.day, 0, 0, 0);
+      List<HealthDataPoint> healthData = await _health.getHealthDataFromTypes(
+        types: types,
+        startTime: start,
+        endTime: now,
+      );
+      setState(() {
+        _stepData = healthData;
+      });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Step Counter')),
+      body: ListView.builder(
+        itemCount: _stepData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Steps: ${_stepData[index].value}'),
+            subtitle: Text('Date: ${_stepData[index].dateFrom}'),
+          );
+        },
+      ),
+    );
   }
 }
